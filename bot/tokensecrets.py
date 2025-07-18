57d94458c481ac616a8379e54e270385e2c5c05e
Alejandro Aldea <alejandro.aldea@usach.cl>
2025-07-18 12:21:16 -0400

    Modificación al Token
    Dividiendo el token en partes para que no me lo bloquee Discord.

--- bot/tokensecrets.py
+++ bot/tokensecrets.py
@@ -1,1 +1,5 @@
-TOKEN = "MTM4MzE1Nzg1ODg1NzkxMDM1Mg.GT7QMn.YcuTC5WbLi-0nXPJzHG-97zjqsfJ9anD-AkDMY"
+parte1 = "MTM4MzE1Nzg1ODg1NzkxMDM1Mg"
+parte2 = "GT7QMn"
+parte3 = "YcuTC5WbLi-0nXPJzHG-97zjqsfJ9anD-AkDMY"
+
+TOKEN = f"{parte1}.{parte2}.{parte3}"
